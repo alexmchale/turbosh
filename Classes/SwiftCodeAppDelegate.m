27e354a8db379bc8c35b411f200f516b411ab9e4
Alex McHale <alexmchale@gmail.com>
2010-03-30 18:17:41 -0500

    Minor code cleanup.

--- Classes/SwiftCodeAppDelegate.m
+++ Classes/SwiftCodeAppDelegate.m
@@ -1,46 +1,33 @@
-//
-//  SwiftCodeAppDelegate.m
-//  SwiftCode
-//
-//  Created by Alex McHale on 3/24/10.
-//  Copyright __MyCompanyName__ 2010. All rights reserved.
-//
-
 #import "SwiftCodeAppDelegate.h"
-
 
 #import "RootViewController.h"
 #import "DetailViewController.h"
 
-#import <sqlite3.h>
-
 @implementation SwiftCodeAppDelegate
 
 @synthesize window, splitViewController, rootViewController, detailViewController, projectSettingsController;
-
 
 #pragma mark -
 #pragma mark Application lifecycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    
+
     // Initialize the database.
     [Store open];
     
     // Add the split view controller's view to the window and display.
     [window addSubview:splitViewController.view];
     [window makeKeyAndVisible];
-    
+
     // Select that last used project and update the DVC to show it.
     self.projectSettingsController = [[[ProjectSettingsController alloc] initWithNibName:nil bundle:nil] autorelease];
     projectSettingsController.proj = [Store currentProject];
     [detailViewController switchTo:projectSettingsController];
-    
+
     return YES;
 
 }
-
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Save data if appropriate
--- FileViewController.m
+++ FileViewController.m
@@ -4,8 +4,9 @@
 
 @synthesize webView, file;
 
- // The designated initializer.  Override if you create the controller programmatically and want to p
- // erform customization that is not appropriate for viewDidLoad.
+// The designated initializer.  Override if you create the controller
+// programmatically and want to perform customization that is not
+// appropriate for viewDidLoad.
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
     }
