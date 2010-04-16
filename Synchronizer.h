90d0603b3de78b2623e1ed869acf4fee063f9aad
Alex McHale <alexmchale@gmail.com>
2010-04-16 03:10:16 -0500

    Continue framework for supporting background synchronization.

--- Synchronizer.h
+++ Synchronizer.h
@@ -1,11 +1,40 @@
 #import <Foundation/Foundation.h>
 
+enum SyncState
+{
+    SS_SELECT_PROJECT,
+    SS_CONNECT_TO_SERVER,
+    SS_SELECT_FILE,
+    SS_INITIATE_HASH,
+    SS_CONTINUE_HASH,
+    SS_COMPLETE_HASH,
+    SS_TEST_IF_CHANGED,
+    SS_INITIATE_UPLOAD,
+    SS_CONTINUE_UPLOAD,
+    SS_COMPLETE_UPLOAD,
+    SS_INITIATE_DOWNLOAD,
+    SS_CONTINUE_DOWNLOAD,
+    SS_COMPLETE_DOWNLOAD
+};
+
 @interface Synchronizer : NSObject
 {
+    enum SyncState state;
+    
     NSTimer *timer;
+    
+    Project *project;
+    ProjectFile *file;
+    
+    NSString *localHash;
+    NSString *remoteHash;
+    
+    NSData *localContent;
 }
 
 @property (nonatomic, retain) NSTimer *timer;
+@property (nonatomic, retain) Project *project;
+@property (nonatomic, retain) ProjectFile *file;
 
 - (void) step;
 
--- Synchronizer.m
+++ Synchronizer.m
@@ -2,14 +2,85 @@
 
 #define SYNCHRONIZE_DELAY_SECONDS 0.25
 
+
 @implementation Synchronizer
 
 @synthesize timer;
+@synthesize project, file;
 
 #pragma mark Synchronizer
 
+- (void) selectProject
+{
+}
+
+- (void) connectToServer
+{
+}
+
+- (void) selectFile
+{
+}
+
+- (void) initiateHash
+{
+}
+
+- (void) continueHash
+{
+}
+
+- (void) completeHash
+{
+}
+
+- (void) testIfChanged
+{
+}
+
+- (void) initiateUpload
+{
+}
+
+- (void) continueUpload
+{
+}
+
+- (void) completeUpload
+{
+}
+
+- (void) initiateDownload
+{
+}
+
+- (void) continueDownload
+{
+}
+
+- (void) completeDownload
+{
+}
+
 - (void) step
 {
+    if (project == nil) state = SS_SELECT_PROJECT;
+    
+    switch (state) {
+        case SS_SELECT_PROJECT:         return [self selectProject];
+        case SS_CONNECT_TO_SERVER:      return [self connectToServer];
+        case SS_SELECT_FILE:            return [self selectFile];
+        case SS_INITIATE_HASH:          return [self initiateHash];
+        case SS_CONTINUE_HASH:          return [self continueHash];
+        case SS_COMPLETE_HASH:          return [self completeHash];
+        case SS_TEST_IF_CHANGED:        return [self testIfChanged];
+        case SS_INITIATE_UPLOAD:        return [self initiateUpload];
+        case SS_CONTINUE_UPLOAD:        return [self continueUpload];
+        case SS_COMPLETE_UPLOAD:        return [self completeUpload];
+        case SS_INITIATE_DOWNLOAD:      return [self initiateDownload];
+        case SS_CONTINUE_DOWNLOAD:      return [self continueDownload];
+        case SS_COMPLETE_DOWNLOAD:      return [self completeDownload];
+    }
 }
 
 #pragma mark Memory Management
@@ -25,12 +96,17 @@
                                    repeats:YES];
     [timer retain];
     
+    project = nil;
+    file = nil;
+    
     return self;
 }
 
 - (void) dealloc
 {
     [timer release];
+    [project release];
+    [file release];
     
     [super dealloc];
 }
