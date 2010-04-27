ce4dc450df3b64528ac3b628da7313a96ac392f6
Alex McHale <alexmchale@gmail.com>
2010-04-27 00:28:05 -0500

    Add supporting code for a timer when the TaskExecController is visible.

--- Classes/TaskExecController.h
+++ Classes/TaskExecController.h
@@ -5,9 +5,11 @@
     UIWebView *webView;
 
     CommandDispatcher *dispatcher;
+    NSTimer *timer;
 }
 
 @property (nonatomic, retain) IBOutlet UIWebView *webView;
 @property (nonatomic, retain) CommandDispatcher *dispatcher;
+@property (nonatomic, retain) NSTimer *timer;
 
 @end
--- Classes/TaskExecController.m
+++ Classes/TaskExecController.m
@@ -2,13 +2,34 @@
 
 @implementation TaskExecController
 
-@synthesize webView, dispatcher;
+@synthesize webView, dispatcher, timer;
+
+- (void) step
+{
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    self.timer = [NSTimer timerWithTimeInterval:0.1 target:self selector:@selector(step) userInfo:nil repeats:YES];
+    [timer release];
+
+    [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+}
+
+- (void) viewDidDisappear:(BOOL)animated
+{
+    [dispatcher close];
+
+    [timer invalidate];
+    self.timer = nil;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     assert(self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]);
 
     dispatcher = nil;
+    timer = nil;
 
     return self;
 }
@@ -20,7 +41,9 @@
 
 - (void)dealloc
 {
+    [webView release];
     [dispatcher release];
+    [timer release];
 
     [super dealloc];
 }
