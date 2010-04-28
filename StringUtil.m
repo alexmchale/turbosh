33ec76955c895973ab45282a676c116d3fe1a22f
Alex McHale <alexmchale@gmail.com>
2010-04-28 06:44:26 -0500

    Start progress on a stringByConvertingAnsiColor method.

--- StringUtil.m
+++ StringUtil.m
@@ -2,6 +2,26 @@
 
 @implementation NSString (monkey)
 
+- (NSString *) stringByConvertingAnsiColor
+{
+    const char *rs = [self cStringUsingEncoding:NSUTF8StringEncoding];
+    char c;
+    bool readingColor = false;
+    bool inColor = false;
+
+    NSMutableString *ms = [NSMutableString string];
+
+    while (c = *rs++) {
+        if (readingColor) {
+        } else {
+        }
+    }
+
+    if (inColor) [ms appendString:@"</span>"];
+
+    return ms;
+}
+
 - (NSString *) stringByQuotingJavascript
 {
     NSString *ns = [self stringByReplacingOccurrencesOfString:@"'" withString:@"\\'"];
