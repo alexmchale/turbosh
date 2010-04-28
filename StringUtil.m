51a5b836b54029f72edf9542a33acba153a5b3d5
Alex McHale <alexmchale@gmail.com>
2010-04-28 07:10:01 -0500

    More bad code for parsin ansi.

--- StringUtil.m
+++ StringUtil.m
@@ -2,18 +2,49 @@
 
 @implementation NSString (monkey)
 
+NSString *color_name(int code)
+{
+    return @"ansiRed";
+}
+
 - (NSString *) stringByConvertingAnsiColor
 {
     const char *rs = [self cStringUsingEncoding:NSUTF8StringEncoding];
     char c;
-    bool readingColor = false;
     bool inColor = false;
+    int code = 0;
+    enum { NORM, OPENTAG, CODE } state;
 
     NSMutableString *ms = [NSMutableString string];
 
     while (c = *rs++) {
-        if (readingColor) {
-        } else {
+        switch (state) {
+            case NORM:
+                if (c != 27) {
+                    [ms appendFormat:@"%c", c];
+                } else {
+                    state = OPENTAG;
+                }
+                break;
+
+            case OPENTAG:
+                if (c == '[') {
+                    state = CODE;
+                    code = 0;
+                }
+                break;
+
+            case CODE:
+                if (c == 'm') {
+                    state = NORM;
+                    if (inColor) [ms appendFormat:@"</span>"];
+                    [ms appendFormat:@"<span class='%@'>", color_name(code)];
+                    inColor = true;
+                } else {
+                    code *= 10;
+                    code += c - '0';
+                }
+                break;
         }
     }
 
